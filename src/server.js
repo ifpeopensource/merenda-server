43635fbd0b1782af3fab4892620adaa269f2200a
Edson Carvalho <77975938+carvalhoedsonn@users.noreply.github.com>
2023-02-16 23:09:25 -0300

    Envio de E-mail (#6)
    * ✨ • Implementation of the email sending system with the QRCode generator
    
    * 🔥
    
    * ✨ • QRcode generator and email sending, refactored
    
    * ✅ Update generateQRCode.js, setup.js and server.js
    
    * ✅ Update generateQRCode.js, setup.js and server.js
    
    * ✅ Update in setup.js
    
    * ✅ Updated server.js, setup.js and file name
    
    * Fix code style issues with ESLint
    
    * ✅ Updated MailService
    
    * Fix code style issues with ESLint
    
    ---------
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import cors from 'cors';
 
@@ -10,5 +13,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000'); 
 });
---
+++ src/services/MailService.js
@@ -0,0 +1,43 @@
+import nodemailer from 'nodemailer';
+
+import { generateStudentQRCode } from '../utils/generateStudentQRCode.js';
+
+
+const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: false,
+    service: process.env.EMAIL_SERVICE,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+async function sendEmail(studentData) {
+
+    const QRCode = await generateStudentQRCode();
+
+    const message = {
+        from: process.env.EMAIL_USER, 
+        to: studentData.email,
+        subject: 'QRCode de Acesso',
+        html: '<p>Este é o seu QRCode de acesso:</p><img src="cid:student_qr_code@ifpeopensource.com.br" alt="QR code">',
+        attachments: [{
+            filename: 'qrcode.png',
+            path: `${QRCode}`,
+            cid: 'student_qr_code@ifpeopensource.com.br'
+        }]
+    };
+
+    transporter.sendMail(message, (error) => {
+        if (error) {
+            console.log(`E-mail não enviado, erro: ${error}`);
+            throw error;
+        } else {
+            console.log('IFOS - E-mail enviado para: ' + message.to);
+        }
+    });
+} 
+
+export default { sendEmail };
