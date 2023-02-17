107c90a3f923d2786af2cd515fa4940e34db6ab6
carvalhoedsonn <edsonjscf@gmail.com>
2023-02-17 13:02:12 -0300

    ✅ • Refactored sendEmail function for reusability

--- src/services/MailService.js
+++ src/services/MailService.js
@@ -1,8 +1,5 @@
 import nodemailer from 'nodemailer';
 
-import { generateStudentQRCode } from '../utils/generateStudentQRCode.js';
-
-
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
@@ -10,26 +7,11 @@ const transporter = nodemailer.createTransport({
   service: process.env.EMAIL_SERVICE,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
-async function sendEmail(studentData) {
-
-  const QRCode = await generateStudentQRCode();
-
-  const message = {
-    from: process.env.EMAIL_USER, 
-    to: studentData.email,
-    subject: 'QRCode de Acesso',
-    html: `<p>Este é o seu QRCode de acesso:</p><img src="cid:student_qr_code@ifpeopensource.com.br" alt="QR code">`,
-    attachments: [{
-      filename: 'qrcode.png',
-      path: `${QRCode}`,
-      cid: 'student_qr_code@ifpeopensource.com.br'
-    }]
-  };
-
+async function sendEmail(message, studentData) {
   transporter.sendMail(message, (error) => {
     if (error) {
       console.log(`E-mail não enviado, erro: ${error}`);
@@ -38,6 +20,6 @@ async function sendEmail(studentData) {
       console.log('IFOS - E-mail enviado para: ' + message.to);
     }
   });
-} 
+}
 
 export default { sendEmail };
