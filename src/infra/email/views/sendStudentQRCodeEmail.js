9a1a400d2c2ca9bdae787aff548a245162590188
Lint Action <lint-action@samuelmeuli.com>
2023-02-18 00:53:22 +0000

    Fix code style issues with ESLint

--- src/infra/email/views/sendStudentQRCodeEmail.js
+++ src/infra/email/views/sendStudentQRCodeEmail.js
@@ -1,25 +1,25 @@
-import sendEmail from "../nodemailer.js";
+import sendEmail from '../nodemailer.js';
 
-import { generateStudentQRCode } from "../../../utils/generateStudentQRCode.js";
+import { generateStudentQRCode } from '../../../utils/generateStudentQRCode.js';
 
 async function sendStudentQRCodeEmail(studentData) {
-  const QRCode = await generateStudentQRCode();
+    const QRCode = await generateStudentQRCode();
 
-  const message = {
-    from: process.env.EMAIL_USER,
-    to: studentData.email,
-    subject: "QRCode",
-    html: '<p>Este é o seu QRCode:</p><img src="cid:student_qr_code@ifpeopensource.com.br" alt="QR code">',
-    attachaments: [
-      {
-        filename: "qrcode.png",
-        path: `${QRCode}`,
-        cid: "student_qr_code@ifpeopensource.com.br",
-      },
-    ],
-  };
+    const message = {
+        from: process.env.EMAIL_USER,
+        to: studentData.email,
+        subject: 'QRCode',
+        html: '<p>Este é o seu QRCode:</p><img src="cid:student_qr_code@ifpeopensource.com.br" alt="QR code">',
+        attachaments: [
+            {
+                filename: 'qrcode.png',
+                path: `${QRCode}`,
+                cid: 'student_qr_code@ifpeopensource.com.br',
+            },
+        ],
+    };
 
-  await sendEmail(message);
+    await sendEmail(message);
 }
 
 export default { sendStudentQRCodeEmail };
